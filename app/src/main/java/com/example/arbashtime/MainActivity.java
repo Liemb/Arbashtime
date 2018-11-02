c2ae75dce255d4e136f58408638490a1a2d2455a
liem <lb6031@bs.amalnet.k12.il>
2018-11-02 18:32:10 +0200

    done

---
+++ app/src/main/java/com/example/arbashtime/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.arbashtime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.btn);
+    }
+
+    public void btlclick(View view) {
+        btn.setText("Oh Yea, I've been clicked!");
+    }
+}
